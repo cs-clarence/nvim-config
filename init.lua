86820e030ef41986ff847040b50890601ba7ff38
Clarence Manuel <rencedm112@gmail.com>
2022-07-26 20:30:19 +0800

    Updated config

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
+-- Initialize the plugins first
+require 'user.plugins'
+
 require 'user.options'
 require 'user.keymaps'
 require 'user.cmp'
 require 'user.lsp'
 require 'user.colorscheme'
-require 'user.plugins'
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,3 +1,11 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd [[packadd packer.nvim]]
+end
+
+
 return require('packer').startup(function()
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
@@ -26,4 +34,10 @@ return require('packer').startup(function()
 
 	-- LSP Installer
 	use 'williamboman/nvim-lsp-installer'
+
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if packer_bootstrap then
+		require('packer').sync()
+	end
 end)
