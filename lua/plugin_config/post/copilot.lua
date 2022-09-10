80ed0c5f0d82124f25264cdd121f546f70d2bb9c
Clarence <rencedm112@gmail.com>
2022-09-10 22:32:37 +0800

    Fixed copilot_cmp

--- lua/plugin_config/post/copilot.lua
+++ lua/plugin_config/post/copilot.lua
@@ -6,11 +6,11 @@ if not copilot_ok then
 end
 
 -- needs to be global then copilot reeds it from the global environment
-copilot_node_command =
-  "/home/rencedm112/.local/share/pnpm/nodejs/16.17.0/bin/node"
+-- copilot_node_command =
+--   "/home/rencedm112/.local/share/pnpm/nodejs/16.17.0/bin/node"
 copilot.setup({})
 
-local copilot_cmp_ok, copilot_cmp = pcall(require, "copilot-cmp")
+local copilot_cmp_ok, copilot_cmp = pcall(require, "copilot_cmp")
 
 if not copilot_cmp_ok then
   vim.notify("Failed to load copilot-cmp")
